f0811e150195e547ee3bd14c95365739dc90526b
Sakura <691863195@qq.com>
2020-12-19 22:27:52 +0800

    first data weather

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.weather;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences prefs = getSharedPreferences("WeatherActivity",Context.MODE_PRIVATE);
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+}
